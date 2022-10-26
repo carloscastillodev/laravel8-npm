558ce3f6be59b346e77e85f6e29dc3d476215b92
Carlos Castillo <ccperalta@gmail.com>
2022-10-26 14:51:15 -0500

    Add Bootstrap Icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header"><i class="bi bi-bootstrap-fill"></i> {{ __('Dashboard') }}</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -95,7 +95,7 @@
                 </div>
             </div>
             <div class="card-footer text-muted">
-                {{ __('You are logged in!') }}
+                {{ __('You are logged in!') }} <i class="fa-solid fa-circle-check"></i>
             </div>
         </div>
     </div>
